8cd0926e537ec480331dbb1b8ac0b804c9eae644
kenoalbers <git@kenoalbers.de>
2025-05-16 16:49:21 +0200

    feat: add flakes

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very complex flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem { 
+      system = "x86_64-linux";
+      modules = [
+	./configuration.nix
+      ];
+    };
+  };
+}
